7d00e1905d901853749c262d599dba98d7c4499d
ikiwi <ikiwicc@gmail.com>
2024-08-03 19:21:00 +0800

    update some api information

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -48,7 +48,7 @@ const fullYear = new Date().getFullYear();
 // 加载配置数据
 const siteStartDate = ref(import.meta.env.VITE_SITE_START);
 const siteIcp = ref(import.meta.env.VITE_SITE_ICP);
-const siteAnthor = ref(import.meta.env.VITE_SITE_ANTHOR);
+const siteAnthor = ref(import.meta.env.VITE_SITE_AUTHOR);
 const siteUrl = computed(() => {
   const url = import.meta.env.VITE_SITE_URL;
   if (!url) return "https://ikiwi.cc";
